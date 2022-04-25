2937efe3b521e699556c3f6f468eced093d8c08d
Manish <Manish@Manish>
2022-04-25 12:09:12 +0530

    By Manish28

--- Github2/src/Demogit1.java
+++ Github2/src/Demogit1.java
@@ -1,9 +1,22 @@
 
 public class Demogit1 {
+	
+	
+	
+	
+	public void m1()
+	{
+		
+		System.out.println("this is method m1");
+		
+	}
 
 	public static void main(String[] args) {
+		
+		Demogit1 demo = new Demogit1();
+		demo.m1();
+		
 		System.out.println("hello git");
-
 	}
 
 }
