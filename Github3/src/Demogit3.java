fa0b015eadd23df3d4516752b8bcbedbd13e4e45
pratik4737 <104188910+pratik4737@users.noreply.github.com>
2022-04-25 13:19:19 +0530

    Update Demogit3.java

--- Github3/src/Demogit3.java
+++ Github3/src/Demogit3.java
@@ -1,9 +1,14 @@
 
 public class Demogit3 {
 
+	
+	public void m1()
+	{
+	System.out.println("m11111");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello github3");
-
+		
 	}
 
 }
